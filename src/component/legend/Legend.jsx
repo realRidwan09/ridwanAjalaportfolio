4bbf9a8e21debf34ded61f34a97c3ac68aae84eb
realRidwan09 <162730097+realRidwan09@users.noreply.github.com>
2024-04-07 06:10:43 -0700

    tes

--- src/component/legend/Legend.jsx
+++ src/component/legend/Legend.jsx
@@ -6,17 +6,19 @@ export default function Legend(){
     return(
         <>
         <div id='container'>
-            <div className="left">
-                <div className="leftWrap">
-                    <span>Hi, I'am </span>
-                    <div>RIDWAN AJALA</div>
-                    <p>I'am Software Developer</p>
-                    <button>Hire me</button>
+            <div className="legenddiv">
+                <div className="left">
+                    <div className="leftWrap">
+                        <h6>Hi, I'am</h6>
+                        <div>RIDWAN AJALA</div>
+                        <p>I'am Software Developer</p>
+                        <button>Hire me</button>
+                    </div>
                 </div>
-            </div>
-            <div className="right">
-                <div className="rightWrap">
-                    <img id='pics' src={picture} alt="picture" />
+                <div className="right">
+                    <div className="rightWrap">
+                        <img id='pics' src={picture} alt="picture" />
+                    </div>
                 </div>
             </div>
         </div>
